b10866d53d1536c25796e7dcb7bed8aa81722b8f
Dego137 <64572465+dego137@users.noreply.github.com.>
2024-08-15 12:59:34 -0500

    algnos ejercicio

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(phrase,number) {
+    let string=''
+    if (number>=0){
+        for(let i=0;i<number;i++){
+            string+=phrase
+        }
+        return string
+    }else{
+        return 'ERROR'
+    }
+    
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(number1,number2) {
+    let sum=0
+    if(Number.isInteger(number1) && Number.isInteger(number2)){
+        if(number1>0 && number2>0){
+            if(number1>number2){
+                for(let i=number2;i<=number1;i++){
+                    sum+=i
+                }
+            }
+            if(number2>number1){
+                for(let i=number1;i<=number2;i++){
+                    sum+=i
+                }
+            }
+            return sum
+        }else{
+            return 'ERROR'
+        }
+    }else return 'ERROR'
 
 };
 
